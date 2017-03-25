3309b97acbfda812e6f66b0eb844c715e74e9279
Michael Wilson <Michael@Athena.local>
2017-03-25 06:49:21 -0400

    New project Reset

--- Dine-O_Xcode/Pods/Target Support Files/Pods-Dine-O/Pods-Dine-O-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dine_O : NSObject
-@end
-@implementation PodsDummy_Pods_Dine_O
-@end
--- Dine-O_Xcode/Pods/Target Support Files/Pods-Dine-OUITests/Pods-Dine-OUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Dine_OUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Dine_OUITests
-@end
