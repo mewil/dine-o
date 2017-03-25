3de786dd58eda91e0837d03751ed4e65b07b4ad3
Noah Momblanco <nmombo@umich.edu>
2017-03-24 23:40:53 -0400

    Add Android and Xcode projects

---
+++ Dine-O_Xcode/Pods/Target Support Files/Pods-Dine-O/Pods-Dine-O-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dine_O : NSObject
+@end
+@implementation PodsDummy_Pods_Dine_O
+@end
---
+++ Dine-O_Xcode/Pods/Target Support Files/Pods-Dine-OTests/Pods-Dine-OTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Dine_OTests : NSObject
+@end
+@implementation PodsDummy_Pods_Dine_OTests
+@end
