695895683f445cf87d63dc7220ad55331829a98e
Michael Wilson <Michael@Athena.local>
2017-03-25 14:22:52 -0400

    Added db stuff

--- response.py
+++ response.py
@@ -20,8 +20,28 @@ def parseMain(sms):
 	sms_items = sms[index_closeB+2:sms.find(']', index_closeB+1)]
 	items = sms_items.split(',')
 	print items
-	#db.#add users
-	#send out surveys
+
+	currentNumbers = client.phone_numbers.list()
+	for currentNumber in currentNumbers:
+		if currentNumber !in db.phoneNumbers:
+			#set phone number and other data, then returns
+			sendReplies(users, currentNumber)
+			return
+
+	#otherwise purchases a new number and sets it and other data
+	purchaseNumbers = client.phone_numbers.search(
+	    area_code="734",
+	    country="US",
+	    type="local"
+	)
+
+	if purchaseNumbers:
+	    purchasedNumber = purchaseNumbers[0].purchase()
+		sendReplies(users, purchasedNumber)
+		db.#add users
+		#send out surveys
+
+	return
 
 
 
@@ -38,3 +58,14 @@ def sendHelpMenu():
 	    	from_= TWILIO_PHONE_NUMBER,
 	    	body = 'USAGE:\n /HELP\n /CURRENT\n /POST [Name][Phone#1,Phone#2,...][FirstItem, SecondItem,...]'
     )
+
+def sendReplies(users, phoneNumber):
+	for user in users;
+		client.messages.create(
+	    	to= user.phoneNumber,
+	    	from_= phoneNumber,
+	    	body = 'USAGE:\n /HELP\n /CURRENT\n /POST [Name][Phone#1,Phone#2,...][FirstItem, SecondItem,...]'
+	    )
+
+
+
