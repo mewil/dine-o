64e7c5d55906eaf6ae76cbb49e7b9e916f470815
Noah Momblanco <nmombo@umich.edu>
2017-03-25 14:14:23 -0400

    add Body parsing

--- response.py
+++ response.py
@@ -1,16 +1,26 @@
 def parseMain(sms):
 	# parse responses
-	# Name,Phone#1,Phone#2,...
-	indices_comma = []
-	for i in range(0, len(sms)):
-		if sms[i] == ',':
-			indices_comma.append(i)
-	numCommas, numPhoneNumbers = len(indices_comma), len(indices_comma)
-	name_primary = sms[:indices_comma[0]]
-	phoneNumbers = []
-	for i in range(0,numCommas):
-		phoneNumbers.append
-	db.#add users
+	# [Name][Phone#1,Phone#2,...][FirstItem,SecondItem,...]
+	# find name
+	index_openB = 0
+	index_closeB = sms.find(']')
+	name = sms[index_openB+1:index_closeB]
+	print name
+	# find phoneNumbers
+	sms_phoneNumbers = sms[index_closeB+2:sms.find(']', index_closeB+1)]
+	phoneNumbers = sms_phoneNumbers.split(',')
+	for i in range(len(phoneNumbers)):
+		if phoneNumbers[i][0] != 1 and len(phoneNumbers[i]) <=10:
+			phoneNumbers[i] = '+1' + phoneNumbers[i]
+		else:
+			phoneNumbers[i] = '+' + phoneNumbers[i]
+	print phoneNumbers
+	# find items
+	index_closeB = sms.find(']', index_closeB+len(sms_phoneNumbers))
+	sms_items = sms[index_closeB+2:sms.find(']', index_closeB+1)]
+	items = sms_items.split(',')
+	print items
+	#db.#add users
 	#send out surveys
 
 
@@ -18,7 +28,7 @@ def parseMain(sms):
 
 def parseResponse(sms):
 	#parse responses
-	db.#add restaurant responses
+	#db.#add restaurant responses
 	#send out decisions if necessary
 
 
@@ -26,5 +36,5 @@ def sendHelpMenu():
 	client.messages.create(
 	    	to= ['From'],
 	    	from_= TWILIO_PHONE_NUMBER,
-	    	body = 'USAGE:\n /HELP\n /CURRENT\n /POST Name,Phone#1,Phone#2,...\n     [FirstItem, SecondItem,...]'
+	    	body = 'USAGE:\n /HELP\n /CURRENT\n /POST [Name][Phone#1,Phone#2,...][FirstItem, SecondItem,...]'
     )
