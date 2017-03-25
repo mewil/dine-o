dc25925b9b8ac9032b777fbc6f80de4271a3602e
Noah Momblanco <nmombo@umich.edu>
2017-03-25 10:21:37 -0400

    update smsParser

---
+++ smsParser_test.py
@@ -0,0 +1,6 @@
+from smsParser import *
+
+# prompt: List your name, the phone numbers of intended recipients and the AA zone
+# format: name, number, number, ..., location
+body = 'Noah Momblanco,12484759744,14759260475,11846374657,South U'
+parseInit(body, '12483217665')
